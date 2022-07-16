f986da54782e44d49b8b7b91e3470f1656b565b5
et42a <101305426+et42a@users.noreply.github.com>
2022-07-16 23:26:56 +0900

    add(bin): install.sh

---
+++ .bin/install.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -ue
+
+link_to_homedir() {
+    local script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
+    local dotdir=$(dirname "${script_dir}")
+
+    for f in "${dotdir}"/.??*; do
+        local bf="$(basename ${f})" # Basename Filename
+        echo $f
+        [[ ${bf} == ".git" ]] && continue
+        [[ ${bf} == ".gitignore" ]] && continue
+        if [[ -L "${HOME}/${bf}" ]]; then # is symlink
+            command rm -f "${HOME}/${bf}"
+        fi
+        command ln -sfnv "${f}" "${HOME}"
+    done
+}
+
+link_to_homedir
