e412864b3c602faacd60abf4a76aa2929517f0b8
et42a <101305426+et42a@users.noreply.github.com>
2022-07-16 23:28:38 +0900

    add(bash): alias.bash

---
+++ .bash/rc/alias.bash
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+echo './bash/aliases'
+
+# Aliases to avoid mistakes
+alias rm='rm -i'
+alias mv='mv -i'
+alias cp='cp -i'
+
+# ls
+alias ls='ls --sort=extension --color=auto'
+alias ll='ls -lh'
+alias la='ls -a'
+
+# grep, fgrep, egrep
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+
--- .bashrc
+++ .bashrc
@@ -2,3 +2,5 @@
 
 # If not running interactivty don't do anything
 [ -z "$PS1" ] && return
+
+source ".bash/rc/alias.bash"
