e256df9b9b02eacc17ee16bd4f2ea5fd8863d518
leviosa42 <101305426+leviosa42@users.noreply.github.com>
2022-11-15 19:22:44 +0900

    update(bin): ignore .gitattributes

--- .bin/install.sh
+++ .bin/install.sh
@@ -10,6 +10,7 @@ link_to_homedir() {
         echo $f
         [[ ${bf} == ".git" ]] && continue
         [[ ${bf} == ".gitignore" ]] && continue
+        [[ ${bf} == ".gitattributes" ]] && continue 
         if [[ -L "${HOME}/${bf}" ]]; then # is symlink
             command rm -f "${HOME}/${bf}"
         fi
