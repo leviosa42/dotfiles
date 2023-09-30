932c18c4ca8209ed3513397c2e1d457b438de310
leviosa42 <101305426+leviosa42@users.noreply.github.com>
2023-09-30 21:41:59 +0900

    update(install.ps1): ipython/jupyter

--- install.ps1
+++ install.ps1
@@ -28,7 +28,7 @@ function Write-Error([string]$message) {
 function New-Symlink([string]$target, [string]$link) {
     if (Test-Path $link) {
         Write-Warning "Remove-Item $link"
-        Remove-Item $link
+        Remove-Item $link -Recurse
     }
     Write-Log "New-Symlink: $link -> $target"
     New-Item -ItemType SymbolicLink -Path $link -Value $target > $null
@@ -36,8 +36,13 @@ function New-Symlink([string]$target, [string]$link) {
 
 function Set-EnvironmentVariable([string]$name, [string]$value) {
     Write-Log "Set-EnvironmentVariable: $name  $value"
+    # if([Environment]::GetEnvironmentVariable($name, "User") -eq $value) {
+    #     return
+    # } else {
+    #     [Environment]::SetEnvironmentVariable($name, $value, "User")
+    # }
     [Environment]::SetEnvironmentVariable($name, $value, "User")
-    # Set-Variable -Name $name -Value $value -Scope Script
+    Invoke-Expression "`$env:${name} = `"$value`""
 }
 
 function New-Directory([string]$path) {
@@ -49,7 +54,7 @@ function New-Directory([string]$path) {
 
 function Install-WinGetPackage([string]$package) {
     Write-Log "Install-WinGetPackage: $package"
-    winget install -e --no-upgrade --accept-source-agreements $package 
+    winget install -e --no-upgrade --accept-source-agreements $package
 }
 
 function Install-ScoopPackage([string]$package) {
@@ -83,12 +88,12 @@ if (Get-Command winget -ErrorAction SilentlyContinue) {
 # check dotfiles is cloned
 if (!(Test-Path $dir_dotfiles)) {
     Write-Info "git clone $url_dotfiles $dir_dotfiles"
-    git clone https://github.com/leviosa42/dotfiles.git $dir_dotfiles
+    git clone "https://github.com/leviosa42/dotfiles.git" $dir_dotfiles
 }
 # dotfiles\.config -> %HOME%\.config
 New-Symlink "$dir_dotfiles\.config" "$dir_home\.config"
 # dotfiles\.config\vim\dot.vimrc -> %HOME%\.vimrc
-New-Symlink "$dir_dotfiles\.config\vim\dot.vimrc" "$dir_home\.vimrc"
+# New-Symlink "$dir_dotfiles\.config\vim\dot.vimrc" "$dir_home\.vimrc"
 # dotfiles\.config\nyagos\dot.nyagos -> %HOME%\.nyagos
 New-Symlink "$dir_dotfiles\.config\nyagos\dot.nyagos" "$dir_home\.nyagos"
 # ====================
@@ -103,19 +108,22 @@ if (!(Get-Command scoop -ErrorAction SilentlyContinue)) {
 # ====================
 # Environment Variables
 # ====================
-# XDG_CONFIG_HOME
-Set-EnvironmentVariable("XDG_CONFIG_HOME", "$dir_home\.config")
-# XDG_CACHE_HOME
-Set-EnvironmentVariable("XDG_CACHE_HOME", "$dir_home\.cache")
-# XDG_DATA_HOME
-Set-EnvironmentVariable("XDG_DATA_HOME", "$dir_home\.local\share")
-New-Directory("$dir_home\.local\share")
-# XDG_STATE_HOME
-Set-EnvironmentVariable("XDG_STATE_HOME", "$dir_home\.local\state")
-New-Directory("$dir_home\.local\state")
-# VIMINIT
-Set-EnvironmentVariable("VIMINIT", 'if !has("nvim") | so $XDG_CONFIG_HOME\vim\dot.vimrc | else | so $XDG_CONFIG_HOME\nvim\init.vim | endif')
+# XDG_Base_Directory
+Set-EnvironmentVariable "XDG_CONFIG_HOME" "$dir_home\.config"
+Set-EnvironmentVariable "XDG_CACHE_HOME" "$dir_home\.cache"
+Set-EnvironmentVariable "XDG_DATA_HOME" "$dir_home\.local\share"
+New-Directory "$dir_home\.local\share"
+Set-EnvironmentVariable "XDG_STATE_HOME" "$dir_home\.local\state"
+New-Directory "$dir_home\.local\state"
+# vim
+Set-EnvironmentVariable "VIMINIT"  "if !has('nvim') | so `$XDG_CONFIG_HOME\vim\dot.vimrc | else | so `$XDG_CONFIG_HOME\nvim\init.vim | endif"
+# ipython/jupiter
+Set-EnvironmentVariable "IPYTHONDIR"  "$env:XDG_CONFIG_HOME\jupyter"
+Set-EnvironmentVariable "JUPYTER_CONFIG_DIR"  "$env:XDG_CONFIG_HOME\jupyter"
 
+# ====================
+# scoop
+# ====================
 # 7zip
 Install-ScoopPackage("7zip")
 # aria2
