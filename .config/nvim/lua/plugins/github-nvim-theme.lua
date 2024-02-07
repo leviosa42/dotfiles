13a86d089037c5fc7337f013ef608b3e6e6eba5e
leviosa42 <101305426+leviosa42@users.noreply.github.com>
2024-02-07 20:17:49 +0900

    update(nvim)

--- .config/nvim/lua/plugins/github-nvim-theme.lua
+++ .config/nvim/lua/plugins/github-nvim-theme.lua
@@ -3,6 +3,53 @@ return { -- colorscheme
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
-        vim.cmd('colorscheme github_dark_default')
-    end
+    -- Default options
+      require('github-theme').setup({
+        options = {
+          -- Compiled file's destination location
+          compile_path = vim.fn.stdpath('cache') .. '/github-theme',
+          compile_file_suffix = '_compiled', -- Compiled file suffix
+          hide_end_of_buffer = true, -- Hide the '~' character at the end of the buffer for a cleaner look
+          hide_nc_statusline = true, -- Override the underline style for non-active statuslines
+          transparent = false,       -- Disable setting background
+          -- transparent = true,
+          terminal_colors = true,    -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
+          dim_inactive = false,      -- Non focused panes set to alternative background
+          -- dim_inactive = true,
+          module_default = true,     -- Default enable value for modules
+          styles = {                 -- Style to be applied to different syntax groups
+            comments = 'NONE',       -- Value is any valid attr-list value `:help attr-list`
+            functions = 'NONE',
+            keywords = 'NONE',
+            variables = 'NONE',
+            conditionals = 'NONE',
+            constants = 'NONE',
+            numbers = 'NONE',
+            operators = 'NONE',
+            strings = 'NONE',
+            types = 'NONE',
+          },
+          inverse = {                -- Inverse highlight for different types
+            match_paren = false,
+            visual = false,
+            search = false,
+          },
+          darken = {                 -- Darken floating windows and sidebar-like windows
+            floats = false,
+            sidebars = {
+              enabled = true,
+              list = {},             -- Apply dark background to specific windows
+            },
+          },
+          modules = {                -- List of various plugins and additional options
+            -- ...
+          },
+        },
+        palettes = {},
+        specs = {},
+        groups = {},
+      })
+    -- setup must be called before loading
+    vim.cmd('colorscheme github_dark_default')
+  end
 }
