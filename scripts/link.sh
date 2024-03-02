4f72351ce382a1fc098e21ae32401a01a6e56a6d
leviosa42 <101305426+leviosa42@users.noreply.github.com>
2024-03-02 19:44:41 +0900

    fix(scripts)

--- scripts/link.sh
+++ scripts/link.sh
@@ -2,9 +2,9 @@
 
 set -euo pipefail
 
-HOME=${HOME:~}
-XDG_CONFIG_HOME=${XDG_CONFIG_HOME:$HOME/.config}
-DOTFILES_ROOTDIR=${DOTFILES_ROOTDIR:$HOME/.dotfiles}
+XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-$HOME/.config}
+XDG_CACHE_HOME=${XDG_CACHE_HOME:-$HOME/.cache}
+DOTFILES_ROOTDIR=${DOTFILES_ROOTDIR:-$HOME/.dotfiles}
 
 linked_source=(
   .config/alacritty/
@@ -21,6 +21,7 @@ linked_source=(
 backup_dir=$XDG_CACHE_HOME/dotfiles/$(date "+%Y%m%d%H%M%S")
 mkdir -p $backup_dir
 
+mkdir -p $XDG_CONFIG_HOME
 for target in "${linked_sources[@]}"; do
   [ -e $XDG_CONFIG_HOME/$target ] && mv $XDG_CONFIG_HOME/$target $backup_dir
   ln -sfnv $DOTFILES_ROOTDIR/$target $XDG_CONFIG_HOME/$target
