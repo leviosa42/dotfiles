45a3ac90f66839814da4546a7f75250e0034589e
leviosa42 <101305426+leviosa42@users.noreply.github.com>
2024-03-02 19:28:35 +0900

    update(bootstrap)

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,17 @@
-#!/usr/bin/sh
+#!/bin/bash
 
+set -euo pipefail
+
+# apt mirror
+# see: https://zenn.dev/ciffelia/articles/c394962a8f188a
+sudo sed -i.bak -r 's@http://(jp\.)?archive\.ubuntu\.com/ubuntu/?@https://ftp.udx.icscoe.jp/Linux/ubuntu/@g' /etc/apt/sources.list
 sudo apt update && sudo apt upgrade -y
 
-sudo apt install git gh -y
+# Install dependencies to clone and run dotfiles/Makefile
+sudo apt install -y git make
 
-cd $HOME
 git clone https://github.com/leviosa42/dotfiles.git ~/.dotfiles
+cd ~/.dotfiles
 
-bash ./.dotfiles/install.sh
+make all
+
