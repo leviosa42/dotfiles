2aaee5c144dc6ab905a556d5f3c3359ea356cb64
leviosa42 <101305426+leviosa42@users.noreply.github.com>
2024-03-02 20:18:25 +0900

    update(scripts)

--- scripts/link.sh
+++ scripts/link.sh
@@ -6,25 +6,32 @@ XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-$HOME/.config}
 XDG_CACHE_HOME=${XDG_CACHE_HOME:-$HOME/.cache}
 DOTFILES_ROOTDIR=${DOTFILES_ROOTDIR:-$HOME/.dotfiles}
 
-linked_source=(
-  .config/alacritty/
-  .config/ash/
-  .config/bash/
-  .config/git/
-  # .config/nyagos/
-  .config/sh/
-  .config/vim/
-  .config/wezterm/
-  # .config/wt/
+sources=(
+  .config/alacritty
+  .config/ash
+  .config/bash
+  .config/git
+  # .config/nyagos
+  .config/sh
+  .config/vim
+  .config/wezterm
+  # .config/wt
 )
 
 backup_dir=$XDG_CACHE_HOME/dotfiles/$(date "+%Y%m%d%H%M%S")
 mkdir -p $backup_dir
 
 mkdir -p $XDG_CONFIG_HOME
-for target in "${linked_sources[@]}"; do
-  [ -e $XDG_CONFIG_HOME/$target ] && mv $XDG_CONFIG_HOME/$target $backup_dir
-  ln -sfnv $DOTFILES_ROOTDIR/$target $XDG_CONFIG_HOME/$target
+for s in "${sources[@]}"; do
+  source=$DOTFILES_ROOTDIR/$s
+	target=${HOME}/${s}
+	echo -e "\033[0;32m$source -> $target\033[0m"
+  if [ -e $target ]; then
+    backupto=$backup_dir/$s
+    mkdir -p $(dirname $backupto)
+    mv $source $backup_dir/$s
+  fi
+  ln -sfn $source $target
 done
 
 # vim: noet
