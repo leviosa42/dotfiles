f454027cab3a7614541de6b1ba14bafc0ac8f226
leviosa42 <101305426+leviosa42@users.noreply.github.com>
2024-03-03 20:26:14 +0900

    update(scripts): non interactive

--- scripts/install_eza.sh
+++ scripts/install_eza.sh
@@ -13,7 +13,7 @@ if [ ! $(command -v gpg &> /dev/null) ]; then \
   sudo apt install -y gpg; \
 fi
 sudo mkdir -p /etc/apt/keyrings
-wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
+wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor --yes -o /etc/apt/keyrings/gierens.gpg
 echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
 sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
 sudo apt update
