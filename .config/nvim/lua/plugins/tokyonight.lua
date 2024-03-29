880031fbe878da87ecb3b5c56295e600f6f05ef5
leviosa42 <101305426+leviosa42@users.noreply.github.com>
2024-03-29 18:25:20 +0900

    'update(nvim)'

--- .config/nvim/lua/plugins/tokyonight.lua
+++ .config/nvim/lua/plugins/tokyonight.lua
@@ -9,8 +9,8 @@ return {
       -- or leave it empty to use the default settings
       style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
       light_style = "day", -- The theme is used when the background is set to light
-      -- transparent = false, -- Enable this to disable setting the background color
-      transparent = true,
+      transparent = false, -- Enable this to disable setting the background color
+      -- transparent = true,
       terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
       styles = {
         -- Style to be applied to different syntax groups
